c940d21dd83aeae0b24362db03dd2d1eb4f208a9
NeonLiryan <67391825+NeonLiryan@users.noreply.github.com>
2022-04-10 19:12:28 +0200

    Interfejs Użytkownika v0.1 (zmieniona cała struktura podstawowego formsa, dodano własne guziki minimalizowania, maksymalizowania i zamykania okna, guzik wczytywania plików, zapisywania (w produkcji), wykresów (w produkcji) oraz możliwość zmiany pozycji okna na ekranie.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,7 +31,13 @@ namespace Geo
         private void InitializeComponent()
         {
             this.map = new GMap.NET.WindowsForms.GMapControl();
-            this.button1 = new System.Windows.Forms.Button();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.MaximalizeButton = new System.Windows.Forms.Button();
+            this.MinimalizeButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Graphs = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // map
@@ -39,13 +45,14 @@ namespace Geo
             this.map.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.map.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.map.Bearing = 0F;
             this.map.CanDragMap = true;
             this.map.EmptyTileColor = System.Drawing.Color.Navy;
             this.map.GrayScaleMode = false;
             this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.map.LevelsKeepInMemory = 5;
-            this.map.Location = new System.Drawing.Point(1, 2);
+            this.map.Location = new System.Drawing.Point(1, 31);
             this.map.MarkersEnabled = true;
             this.map.MaxZoom = 2;
             this.map.MinZoom = 2;
@@ -59,31 +66,109 @@ namespace Geo
             this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.map.ShowTileGridLines = false;
-            this.map.Size = new System.Drawing.Size(497, 445);
+            this.map.Size = new System.Drawing.Size(1100, 560);
             this.map.TabIndex = 0;
             this.map.Zoom = 0D;
             this.map.DoubleClick += new System.EventHandler(this.map_DoubleClick);
             this.map.MouseClick += new System.Windows.Forms.MouseEventHandler(this.map_MouseClick);
             // 
-            // button1
+            // LoadButton
             // 
-            this.button1.Location = new System.Drawing.Point(585, 104);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.LoadButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LoadButton.Location = new System.Drawing.Point(1, 0);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(105, 31);
+            this.LoadButton.TabIndex = 1;
+            this.LoadButton.Text = "Wczytaj";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.CloseButton.ForeColor = System.Drawing.Color.Red;
+            this.CloseButton.Location = new System.Drawing.Point(1061, 0);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(40, 31);
+            this.CloseButton.TabIndex = 2;
+            this.CloseButton.Text = "X";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MaximalizeButton
+            // 
+            this.MaximalizeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.MaximalizeButton.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.MaximalizeButton.Location = new System.Drawing.Point(1023, 0);
+            this.MaximalizeButton.Name = "MaximalizeButton";
+            this.MaximalizeButton.Size = new System.Drawing.Size(40, 31);
+            this.MaximalizeButton.TabIndex = 3;
+            this.MaximalizeButton.Text = "[_]";
+            this.MaximalizeButton.UseVisualStyleBackColor = true;
+            this.MaximalizeButton.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // MinimalizeButton
+            // 
+            this.MinimalizeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.MinimalizeButton.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.MinimalizeButton.Location = new System.Drawing.Point(984, 0);
+            this.MinimalizeButton.Name = "MinimalizeButton";
+            this.MinimalizeButton.Size = new System.Drawing.Size(40, 31);
+            this.MinimalizeButton.TabIndex = 4;
+            this.MinimalizeButton.Text = "__";
+            this.MinimalizeButton.UseVisualStyleBackColor = true;
+            this.MinimalizeButton.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.SaveButton.Location = new System.Drawing.Point(105, 0);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(105, 31);
+            this.SaveButton.TabIndex = 5;
+            this.SaveButton.Text = "Zapisz";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(985, 31);
+            this.panel1.TabIndex = 6;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // Graphs
+            // 
+            this.Graphs.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Graphs.Location = new System.Drawing.Point(210, 0);
+            this.Graphs.Name = "Graphs";
+            this.Graphs.Size = new System.Drawing.Size(105, 31);
+            this.Graphs.TabIndex = 7;
+            this.Graphs.Text = "Wykresy";
+            this.Graphs.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(1100, 590);
+            this.Controls.Add(this.Graphs);
+            this.Controls.Add(this.MinimalizeButton);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.MaximalizeButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.map);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.ShowIcon = false;
+            this.Text = "EdytorGPX";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
@@ -92,7 +177,13 @@ namespace Geo
         #endregion
 
         private GMap.NET.WindowsForms.GMapControl map;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button MaximalizeButton;
+        private System.Windows.Forms.Button MinimalizeButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Graphs;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Globalization;
 using System.Linq;
@@ -15,6 +16,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Xml;
 using U8Xml;
 
@@ -23,38 +25,63 @@ namespace Geo
 {
     public partial class Form1 : Form
     {
+        private bool dragging = false;
+        private Point startPoint = new Point(0, 0);
 
         double lat = 0;
         double lng = 0;
 
+
         public Form1()
         {
             InitializeComponent();
-            
+            LoadButton.FlatStyle = FlatStyle.Flat;
+            LoadButton.FlatAppearance.BorderSize = 0;
+            LoadButton.BackColor = System.Drawing.Color.Gray;
+            SaveButton.FlatStyle = FlatStyle.Flat;
+            SaveButton.FlatAppearance.BorderSize = 0;
+            SaveButton.BackColor = System.Drawing.Color.Gray;
+            Graphs.FlatStyle = FlatStyle.Flat;
+            Graphs.FlatAppearance.BorderSize = 0;
+            Graphs.BackColor = System.Drawing.Color.Gray;
+            CloseButton.FlatStyle = FlatStyle.Flat;
+            CloseButton.FlatAppearance.BorderSize = 0;
+            CloseButton.BackColor = System.Drawing.Color.Gray;
+            MaximalizeButton.FlatStyle = FlatStyle.Flat;
+            MaximalizeButton.BackColor = System.Drawing.Color.Gray;
+            MaximalizeButton.FlatAppearance.BorderSize = 0;
+            MinimalizeButton.FlatStyle = FlatStyle.Flat;
+            MinimalizeButton.BackColor = System.Drawing.Color.Gray;
+            MinimalizeButton.FlatAppearance.BorderSize = 0;
         }
+
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
             map.Position = new GMap.NET.PointLatLng(51.76, 19.45);
             map.DragButton = MouseButtons.Right;
             map.MapProvider = GMapProviders.GoogleMap;
             map.MinZoom = 10;
-            map.MaxZoom =100;
+            map.MaxZoom = 100;
             map.Zoom = 10;
             map.ShowCenter = false;
+
+
 
 
         }
 
         private void map_DoubleClick(object sender, EventArgs e)
         {
-            
+
         }
 
         private void map_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
         {
 
-            
+
             if (e.Button == System.Windows.Forms.MouseButtons.Left)
             {
                 lat = map.FromLocalToLatLng(e.X, e.Y).Lat;
@@ -63,29 +90,76 @@ namespace Geo
 
             var point = new GMap.NET.PointLatLng(lat, lng);
             GMap.NET.WindowsForms.GMapMarker marker = new GMarkerGoogle(point, GMarkerGoogleType.red);
-           // map.Overlays.Clear();
+            // map.Overlays.Clear();
             GMapOverlay markers = new GMapOverlay("markers");
-            map.Overlays.Add(markers);      
+            map.Overlays.Add(markers);
             markers.Markers.Add(marker);
 
-          
 
-           
-           
+
+
+
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            XmlWrapper wrapper = new XmlWrapper("PlikDoTestow.gpx");
-            List<Punkt> punkty = wrapper.ReadTrk();
-            FileMetadata filemetadata = wrapper.ReadMetadata();
 
-            Console.WriteLine(filemetadata.GetData()); //:)
+            Process.Start("c:\\");
+
             
-            foreach(Punkt data in punkty)
+             XmlWrapper wrapper = new XmlWrapper("PlikDoTestow.gpx");
+             List<Punkt> punkty = wrapper.ReadTrk();
+             FileMetadata filemetadata = wrapper.ReadMetadata();
+
+             Console.WriteLine(filemetadata.GetData()); //:)
+
+            foreach (Punkt data in punkty)
             {
                 Console.WriteLine(data.GetData());
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (WindowState.ToString() == "Normal")
+            {
+                WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void panel1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            dragging = true;
+            startPoint = new Point(e.X, e.Y);
+        }
+
+        private void panel1_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            dragging = false;
+        }
+
+        private void panel1_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if(dragging)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - startPoint.X, p.Y - startPoint.Y);
             }
         }
     }
